4f25aebcaa5c771f7ef163c49e7837bbac612c2f
Carmy93 <carmygargiulo45@gmail.com>
2024-09-30 16:43:22 +0200

    Esercizio 3 completo

--- Day 1/src/Esercizio3/Main.java
+++ Day 1/src/Esercizio3/Main.java
@@ -3,9 +3,9 @@ package Esercizio3;
 public class Main {
     public static void main(String[] args){
         ContoCorrente mio = new ContoCorrente("Carmine", 100);
-        ContoOnLine mio2 = new ContoOnLine("Carmine", 15000, 500);
+        ContoOnLine mio2 = new ContoOnLine("Carmine", 400, 500);
         mio2.preleva(600);
-        mio.preleva(150);
+        mio2.preleva(500);
         System.out.println(mio2.restituisciSaldo());
     }
 }
