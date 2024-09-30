4bf74c49c83e98c1e2129d1a8cc9b32d04e2dd8e
Carmy93 <carmygargiulo45@gmail.com>
2024-09-30 15:57:24 +0200

    Esercizio 2 completo

--- Day 1/src/Esercizio2/Main.java
+++ Day 1/src/Esercizio2/Main.java
@@ -1,7 +1,24 @@
 package Esercizio2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Inserisci i km percorsi");
+            String km = sc.nextLine();
+            System.out.println("Adesso inserisci i litri di carburante consumati");
+            String litri = sc.nextLine();
+            try {
+                int rapporto = (Integer.parseInt(km)/Integer.parseInt(litri));
+                System.out.println("Il rapporto km/litri consumati é: " + rapporto);
+                break;
+            } catch (NumberFormatException ex){
+                System.out.println("Non hai inserito un numero");
+            } catch (ArithmeticException e) {
+                System.out.println("Impossibile dividere per zero");
+            }
+        }
     }
 }
