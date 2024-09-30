28c353a90b39e402b59a93b5306d6749189dd975
Carmy93 <carmygargiulo45@gmail.com>
2024-09-30 17:59:16 +0200

    Esercizio 3 completo

--- Day 1/src/Esercizio1/RandomArray.java
+++ Day 1/src/Esercizio1/RandomArray.java
@@ -1,9 +1,13 @@
 package Esercizio1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomArray {
+    private static final Logger logger = LoggerFactory.getLogger(RandomArray.class);
    public int[] randoms = new int[5];
 
     public RandomArray(){
@@ -28,11 +32,13 @@ public class RandomArray {
             System.out.println("Indica la posizione nella quale inserire un numero casuale 0 per terminare");
             String x = sc.nextLine();
             try {
+                logger.debug("Sto provando a parsare");
                 if(Integer.parseInt(x) == 0) break;
                 randoms[Integer.parseInt(x)-1] = (int) (Math.random()* 10) + 1;
                 System.out.println("Il nuovo array è: ");
                 System.out.println(toString());
             } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+                logger.error("Errore della lunghezza array");
                 System.out.println(ex.getMessage());
                 System.out.println("Riprova");
             }
